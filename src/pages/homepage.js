90719698e77f1481339a56feb9af8a7ec203ad88
ebenKB <eakbo23@gmail.com>
2019-12-02 13:45:06 +0000

    refactor homepage into components add style to menu item components

---
+++ src/pages/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../stylesheet/homepage-style.scss';
+import Directory from '../components/directory/directory';
+
+const Homepage = () => {
+return(
+  <div className="homepage">
+    <Directory />
+  </div>
+)
+}
+
+export default Homepage
